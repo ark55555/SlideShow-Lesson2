e04ac997480343e3ec1c75be473eecf4a0c689d9
ArikaWakasugi <ark.tooyoung@gmail.com>
2021-06-30 17:14:41 +0900

    [Add]Ajax-api-weather

--- js/script.js
+++ js/script.js
@@ -53,3 +53,36 @@ $('#tab-menu a').on('click', function(event){
   $($(this).attr("href")).show();
   event.preventDefault();
 });
+
+// 取得したAPIキー
+const API_KEY ="1a2503652ce880c2c7dd18e58428d9e0";
+
+$(function(){
+  $('#btn').on('click',function(){
+    // 入力された都市名でWebAPIに天気情報をリクエスト
+    $.ajax({
+      url: "https://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
+      dateType:'jsonp',
+    }).done(function(date){
+      //通信成功
+      // 位置
+      $('#place').text(date.name);
+      // 最高気温
+      $('#temp_max').text(date.main.temp_max);
+      // 最低気温
+      $('#temp_min').text(date.main.temp_min);
+      // 湿度
+      $('#humidity').text(date.main.humidity);
+      // 風速
+      $('#speed').text(date.wind.speed);
+      // 天気
+      $('#weather').text(date.weather[0].main);
+      // 天気アイコン
+      $('#img').attr("src","http://openweathermap.org/img/w/" + date.weather[0].icon + ".png");
+      $('#img').attr("alt",date.weather[0].main);
+    }).fail(function(date){
+      //通信失敗
+      alert('通信に失敗しました。');
+    });
+  });
+});
